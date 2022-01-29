d77fcb2c18112437f321d609a45ebc079b509e46
Harimad <leesh68481@gmail.com>
2022-01-29 15:46:13 +0900

    #7.2 Coin Tracker

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,43 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
-  const onChange = event => setToDo(event.target.value);
-  const onSubmit = event => {
-    event.preventDefault();
-    if (toDo === '') {
-      return;
-    }
-    setToDos(currentArray => [toDo, ...currentArray]);
-    setToDo('');
-    console.log(toDos);
-    console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
-  };
-  const deleteBtn = index => {
-    setToDos(toDos.filter((item, todoIndex) => index !== todoIndex));
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your TODO"
-        ></input>
-        <button>Add To Do</button>
-      </form>
-      <hr></hr>
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button onClick={() => deleteBtn(index)}>❌</button>
-          </li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? '' : coins.length}</h1>
+      {loading ? (
+        <strong>Loding...</strong>
+      ) : (
+        <select>
+          {coins.map(coin => (
+            <option>
+              {coin.name}({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
