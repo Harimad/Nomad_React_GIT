d2028be741a0e3c49e82fa1621a00a97b3778df1
Harimad <leesh68481@gmail.com>
2022-01-30 16:00:11 +0900

    #7.5 React Router

--- react-for-beginners/src/components/Movie.js
+++ react-for-beginners/src/components/Movie.js
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>{genres && genres.map(g => <li key={g}>{g}</li>)}</ul>
       {/* genres가 있는지 먼저 체크해주고 genres를 map으로 그려줘야지 에러가 안뜸 */}
--- react-for-beginners/src/routes/Home.js
+++ react-for-beginners/src/routes/Home.js
@@ -1,44 +1,47 @@
 import { useEffect, useState } from 'react';
 import Movie from '../components/Movie';
 
-const [loading, setLoading] = useState(true);
-const [movies, setMovies] = useState([]);
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
 
-const getMovies = async function () {
-  const response = await fetch(
-    `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-  );
-  const json = await response.json();
+  const getMovies = async function () {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    );
+    const json = await response.json();
 
-  setMovies(json.data.movies);
-  setLoading(false);
-};
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
 
-useEffect(() => {
-  getMovies();
-}, []);
+  useEffect(() => {
+    getMovies();
+  }, []);
 
-return (
-  <div>
-    {loading ? (
-      <h1>Loading... </h1>
-    ) : (
-      <div>
-        (
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading... </h1>
+      ) : (
         <div>
-          {movies.map(movie => (
-            <Movie
-              key={movie.id}
-              coverImg={movie.medium_cover_img}
-              title={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
+          (
+          <div>
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                coverImg={movie.medium_cover_image}
+                title={movie.title}
+                summary={movie.summary}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+          )
         </div>
-        )
-      </div>
-    )}
-  </div>
-);
+      )}
+    </div>
+  );
+}
 
 export default Home;
