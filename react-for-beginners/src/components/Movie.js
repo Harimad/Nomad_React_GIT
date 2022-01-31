361eb2e1b769069abe1e2347428a7ac835e2632c
Harimad <leesh68481@gmail.com>
2022-01-31 13:52:33 +0900

    #7.6 Parameters

--- react-for-beginners/src/components/Movie.js
+++ react-for-beginners/src/components/Movie.js
@@ -15,6 +15,7 @@ function Movie({ id, coverImg, title, summary, genres }) {
   );
 }
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
